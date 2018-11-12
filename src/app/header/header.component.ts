614a72e16773ffbeba1a0eb4a0d8672e60029b2a
amangaur95 <amankumargaur@gmail.com>
2018-11-12 10:43:51 +0530

    package1

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private userservice:UserService,
+  constructor(public userservice:UserService,
     private router:Router) { 
 
     }
